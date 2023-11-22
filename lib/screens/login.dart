2c60478c5cddf2071c992226294959d852b02e88
Farah <farahramadhinaa@gmail.com>
2023-11-22 14:13:22 +0700

    Buat Tutorial 9

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -2,7 +2,6 @@ import 'package:shopping_list/screens/menu.dart';
 import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
-import 'package:shopping_list/screens/login.dart';
 
 void main() {
   runApp(const LoginApp());
